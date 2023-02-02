e2cad7d8379297db8358d912cc575958334f80b1
Bouxy99 <62006954+Bouxy99@users.noreply.github.com>
2023-02-02 17:04:03 +0100

    Dev 5

---
+++ js/init.js
@@ -0,0 +1,28 @@
+
+let scroll_actu = 0;
+let step_size = 3;
+
+let div_dict = {
+    "day1": {},
+    "day2": {},
+    "day3": {},
+    "day4": {},
+    "day5": {},
+    "day6": {},
+    "day7": {},
+    "day8": {},
+    "day9": {},
+    "day10": {},
+    "day11": {},
+    "day12": {},
+    "day13": {},
+    "day14": {},
+    "day15": {},
+    "day16": {},
+    "day17": {},
+    "day18": {},
+    "day19": {},
+    "day20": {},
+    "day21": {},
+    "day22": {}
+};
--- js/scroll_event.js
+++ js/scroll_event.js
@@ -1,7 +1,5 @@
 // Scroll event
 let i = 0;
-let scroll_actu = 0;
-let step_size = 3;
 addEventListener("scroll", (event) => {
     if (scroll_actu < window.scrollY) {
         if (i < coordinates.length) {
@@ -27,30 +25,19 @@ addEventListener("scroll", (event) => {
     updateDiv();
 });
 
-div_dict = {
-    "day1": {},
-    "day2": {},
-    "day3": {},
-    "day4": {},
-    "day5": {},
-    "day6": {},
-    "day7": {},
-    "day8": {},
-    "day9": {},
-    "day10": {},
-    "day11": {},
-    "day12": {},
-    "day13": {},
-    "day14": {},
-    "day15": {},
-    "day16": {},
-    "day17": {},
-    "day18": {},
-    "day19": {},
-    "day20": {},
-    "day21": {},
-    "day22": {}
-};
+//Show map after scrolling
+$(window).scroll(function () {
+    d_top_map = $('#map').offset().top;
+    console.log($(this).scrollTop()-d_top_map)
+    if ((d_top_map - $(this).scrollTop()) <= 0) {
+        $('#map').removeClass('map_abs');
+        $('#map').addClass('map_fix');
+    }
+    else {
+        $('#map').removeClass('map_fix');
+        $('#map').addClass('map_abs');
+    }
+});
 
 function updateDiv() {
     for (const [key, value] of Object.entries(div_dict)) {
@@ -62,7 +49,7 @@ function updateDiv() {
 
         // Find active div
         $("#" + key + "_card").removeClass('active_day');
-        if (d_top <= (window.innerHeight/2) && (d_top+height) >= (window.innerHeight/2)) {
+        if (d_top <= (window.innerHeight / 2) && (d_top + height) >= (window.innerHeight / 2)) {
             active_div = key;
             $("#" + key + "_card").addClass('active_day');
         }
